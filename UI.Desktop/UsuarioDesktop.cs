d94e22c644e60ae4e9752187429ddf0ede4bdd45
FacuTaborra <taborrafacundo@gmail.com>
2022-06-04 08:59:56 -0300

    nada

--- UI.Desktop/UsuarioDesktop.cs
+++ UI.Desktop/UsuarioDesktop.cs
@@ -35,7 +35,6 @@ namespace UI.Desktop
                 _UsuarioActual = ul.GetOne(ID);
                 MapearDeDatos();
             }
-            
             if(_Modo == ModoForm.alta || _Modo == ModoForm.modificacion)
             {
                 this.btnAceptar.Text = "Guardar";
