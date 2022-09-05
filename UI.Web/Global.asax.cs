a4afcd7d91c6c864ecc38838467518931ad1c8cd
lucia cappll <luciacappll@gmail.com>
2022-09-05 17:01:31 -0300

    Agrego proyecto UI.Web

--- UI.Web/Global.asax.cs
+++ UI.Web/Global.asax.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
 
 namespace UI.Web
 {
-    public class Global : System.Web.HttpApplication
+    public class Global : HttpApplication
     {
-        protected void Application_Start(object sender, EventArgs e)
+        void Application_Start(object sender, EventArgs e)
         {
+            // Code that runs on application startup
+           AreaRegistration.RegisterAllAreas();
+           RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
 }
--- UI.Web/Properties/AssemblyInfo.cs
+++ UI.Web/Properties/AssemblyInfo.cs
@@ -2,34 +2,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// La información general sobre un ensamblado se controla mediante el siguiente 
-// conjunto de atributos. Cambie los valores de estos atributos para modificar la información
-// asociada a un ensamblado.
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
 [assembly: AssemblyTitle("UI.Web")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("HP Inc.")]
 [assembly: AssemblyProduct("UI.Web")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Si ComVisible se establece en false, los componentes COM no verán los 
-// tipos de este ensamblado. Si necesita obtener acceso a un tipo de este ensamblado desde 
-// COM, establezca el atributo ComVisible en true en este tipo.
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// El siguiente GUID es para el Id. typelib cuando este proyecto esté expuesto a COM
-[assembly: Guid("01cb04f2-c126-49b4-bc8b-dff8ed1e7765")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b46c080b-93d2-4661-b508-72b836ca6a32")]
 
-// La información de versión de un ensamblado consta de los siguientes cuatro valores:
+// Version information for an assembly consists of the following four values:
 //
-//      Versión principal
-//      Versión secundaria 
-//      Número de compilación
-//      Revisión
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
 //
-// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión 
-// mediante el carácter '*', como se muestra a continuación:
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
