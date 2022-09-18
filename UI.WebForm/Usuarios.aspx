a92951cfc02ab0e53e135f3eab43b88eb06bfa73
lucia cappll <luciacappll@gmail.com>
2022-09-18 09:30:07 -0300

    ABMC Usuarios - WebForm

---
+++ UI.WebForm/Usuarios.aspx
@@ -0,0 +1,54 @@
+﻿<%@ Page Title="Usuarios" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Usuarios.aspx.cs" Inherits="UI.WebForm.Usuarios" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="bodyContentPlaceHolder" runat="server">
+
+    <asp:Panel ID="gridPanel" runat="server">
+        <asp:GridView  ID="gridView" runat="server" AutoGenerateColumns="false" SelectedRowStyle-BackColor="Black" SelectedRowStyle-ForeColor="White" DataKeyNames="ID" OnSelectedIndexChanged="gridView_SelectedIndexChanged">
+            <Columns>
+                <asp:BoundField HeaderText="ID" DataField="ID"/>
+                <asp:BoundField HeaderText="Nombre" DataField="Nombre"/>
+                <asp:BoundField HeaderText="Apellido" DataField="apellido"/>
+                <asp:BoundField HeaderText="EMail" DataField="Email"/>
+                <asp:BoundField HeaderText="Usuario" DataField="NombreUsuario"/>
+                <asp:BoundField HeaderText="Habilitado" DataField="Habilitado"/>
+                <asp:CommandField SelectText="Seleccioar" ShowSelectButton="true"/>
+            </Columns>
+        </asp:GridView>
+    </asp:Panel>
+
+    <asp:Panel ID="gridActionPanel" runat="server">
+        <asp:LinkButton ID="nuevoLinkButton" runat="server" OnClick="nuevoLinkButton_Click">Nuevo</asp:LinkButton>
+        <asp:LinkButton ID="editarLinkButton" runat="server" OnClick="editarLinkButton_Click">Editar</asp:LinkButton>
+        <asp:LinkButton ID="eliminarLinkButton" runat="server" OnClick="eliminarLinkButton_Click">Eliminar</asp:LinkButton>
+    </asp:Panel>
+
+
+    <asp:Panel ID="formPanel" Visible="false" runat="server">
+        <asp:Label ID="nombreLabel" runat="server" Text="Nombre: " ></asp:Label>
+        <asp:TextBox ID="nombreTextBox" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="apellidoLabel" runat="server" Text="Apellido: "></asp:Label>
+        <asp:TextBox ID="apellidoTextBox" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="emailLabel" runat="server" Text="EMail: "></asp:Label>
+        <asp:TextBox ID="emailTextBox" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="habilitadoLabel" runat="server" Text="Habilitado: "></asp:Label>
+        <asp:CheckBox ID="habilitadoCheckBox" runat="server" />
+        <br />
+        <asp:Label ID="nombreUsuarioLabel" runat="server" Text="Usuario: "></asp:Label>
+        <asp:TextBox ID="nombreUsuarioTextBox" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="claveLabel" runat="server" Text="Clave:"></asp:Label>
+        <asp:TextBox ID="claveTextBox" runat="server" TextMode="Password"></asp:TextBox>
+        <br />
+        <asp:Label ID="repetirClaveLabel" runat="server" Text="Repetir Clave:"></asp:Label>
+        <asp:TextBox ID="repetirClaveTextBox" runat="server" TextMode="Password"></asp:TextBox>
+        <br />
+        <asp:Panel ID="formActionPanel" runat="server">
+            <asp:LinkButton ID="aceptarLinkButton" runat="server" OnClick="aceptarLinkButton_Click">Aceptar</asp:LinkButton>
+            <asp:LinkButton ID="cancelarLinkButton" runat="server" OnClick="cancelarLinkButton_Click">Cancelar</asp:LinkButton>
+    </asp:Panel>
+    </asp:Panel>
+
+
+</asp:Content>
---
+++ UI.WebForm/Usuarios.aspx.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Business.Entities;
+using Business.Logic;
+
+namespace UI.WebForm
+{
+    public partial class Usuarios : System.Web.UI.Page
+    {
+        UsuarioLogic _logic;
+
+        private UsuarioLogic Logic
+        {
+            get
+            {
+                if (_logic == null)
+                {
+                    _logic = new UsuarioLogic();
+                }
+                return _logic;
+            }
+        }
+
+        public enum FormModes
+        {
+            Alta,
+            Baja,
+            Modificacion
+        }
+
+        public FormModes FormMode
+        {
+            get { return (FormModes)this.ViewState["FormMode"]; }
+            set { this.ViewState["FormMode"] = value; }
+        }
+
+        private Usuario Entity
+        {
+            get;
+            set;
+        }
+
+        private int SelectedID
+        {
+            get
+            {
+                if (this.ViewState["SelectedID"] != null)
+                {
+                    return (int)this.ViewState["SelectedID"];
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            set
+            {
+                this.ViewState["SelectedID"] = value;
+            }
+        }
+
+        private bool IsEntitySelected
+        {
+            get
+            {
+                return (this.SelectedID != 0);
+            }
+        }
+
+        private void LoadGrid()
+        {
+            this.gridView.DataSource = this.Logic.GetAll();
+            this.gridView.DataBind();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.Page.IsPostBack)
+            {
+                LoadGrid();
+            }
+        }
+
+        protected void gridView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.SelectedID = (int)this.gridView.SelectedValue;
+        }
+
+        private void LoadForm(int id)
+        {
+            this.Entity = this.Logic.GetOne(id);
+            this.nombreTextBox.Text = Entity.Nombre;
+            this.apellidoTextBox.Text = Entity.Apellido;
+            this.emailTextBox.Text = Entity.Email;
+            this.habilitadoCheckBox.Checked = this.Entity.Habilitado;
+            this.nombreUsuarioTextBox.Text = this.Entity.NombreUsuario;
+        }
+
+        private void LoadEntity(Usuario usuario)
+        {
+            usuario.Nombre = this.nombreTextBox.Text;
+            usuario.Apellido = this.apellidoTextBox.Text;
+            usuario.Email = this.emailTextBox.Text;
+            usuario.NombreUsuario = this.nombreUsuarioTextBox.Text;
+            usuario.Clave = this.claveTextBox.Text;
+            usuario.Habilitado = this.habilitadoCheckBox.Checked;
+        }
+
+        private void SaveEntity(Usuario usr)
+        {
+            //this.Logic.Save(usr);
+            this.Logic.Save(usr);
+        }
+
+        private void DeleteEntity(int id)
+        {
+            this.Logic.Delete(id);
+        }
+
+        private void EnableForm(bool enable)
+        {
+            this.nombreTextBox.Enabled = enable;
+            this.apellidoTextBox.Enabled = enable;
+            this.emailTextBox.Enabled = enable;
+            this.nombreUsuarioTextBox.Enabled = enable;
+            this.claveTextBox.Visible = enable;
+            this.claveLabel.Visible = enable;
+            this.repetirClaveLabel.Visible = enable;
+            this.repetirClaveTextBox.Visible = enable;
+        }
+
+        protected void aceptarLinkButton_Click(object sender, EventArgs e)
+        {
+            switch (this.FormMode)
+            {
+                case FormModes.Baja:
+                    this.DeleteEntity(this.SelectedID);
+                    this.LoadGrid();
+                    break;
+                case FormModes.Modificacion:
+                    this.Entity = new Usuario();
+                    this.Entity.ID = this.SelectedID;
+                    this.Entity.State = BusinessEntity.States.Modified;
+                    this.LoadEntity(this.Entity);
+                    this.SaveEntity(this.Entity);
+                    this.LoadGrid();
+                    break;
+                case FormModes.Alta:
+                    this.Entity = new Usuario();
+                    this.Entity.State = BusinessEntity.States.New;
+                    this.LoadEntity(this.Entity);
+                    this.SaveEntity(this.Entity);
+                    this.LoadGrid();
+                    break;
+                default:
+                    break;
+            }
+            this.formPanel.Visible = false;
+        }
+
+
+        protected void editarLinkButton_Click(object sender, EventArgs e)
+        {
+            if (this.IsEntitySelected)
+            {
+                this.formPanel.Visible = true;
+                this.FormMode = FormModes.Modificacion;
+                this.LoadForm(this.SelectedID);
+                this.EnableForm(true);
+            }
+        }
+
+        protected void eliminarLinkButton_Click(object sender, EventArgs e)
+        {
+            if (this.IsEntitySelected)
+            {
+                this.formPanel.Visible = true;
+                this.FormMode = FormModes.Baja;
+                this.EnableForm(false);
+                this.LoadForm(this.SelectedID);
+            }
+        }
+
+        protected void nuevoLinkButton_Click(object sender, EventArgs e)
+        {
+            this.formPanel.Visible = true;
+            this.FormMode = FormModes.Alta;
+            this.ClearForm();
+            this.EnableForm(true);
+        }
+
+        protected void cancelarLinkButton_Click(object sender, EventArgs e)
+        {
+            this.LoadGrid();
+            this.formPanel.Visible = false;
+        }
+
+        private void ClearForm()
+        {
+            this.nombreTextBox.Text = string.Empty;
+            this.apellidoTextBox.Text = string.Empty;
+            this.emailTextBox.Text = string.Empty;
+            this.habilitadoCheckBox.Checked = false;
+            this.nombreUsuarioTextBox.Text = string.Empty;
+        }
+
+    }
+}
