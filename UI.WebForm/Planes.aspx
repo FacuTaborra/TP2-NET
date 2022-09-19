03100a3f0347e18df1043d3be1c1cbfec0e3db25
FacuTaborra <taborrafacundo@gmail.com>
2022-09-19 01:05:15 -0300

    agrego planes a Web Form

---
+++ UI.WebForm/Planes.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Planes.aspx.cs" Inherits="UI.Web.Planes" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="bodyContentPlaceHolder" runat="server">
+     <asp:Panel ID="gridPanel" runat="server">
+       <asp:GridView ID="gridView" runat="server" AutoGenerateColumns="False" DataKeyNames="ID" SelectedRowStyle-BackColor="Black" SelectedRowStyle-ForeColor="White"  OnSelectedIndexChanged="gridView_SelectedIndexChanged" BackColor="White" BorderColor="#CC9966" BorderStyle="None" BorderWidth="1px" CellPadding="4" Height="206px" Width="488px">
+           <Columns>
+               <asp:BoundField HeaderText="Id Plan" DataField="ID"/>
+               <asp:BoundField HeaderText="Descripcion" DataField="Descripcion"/>
+               <asp:BoundField HeaderText="Id Especialidad" DataField="IDEspecialidad"/>
+               <asp:CommandField SelectText="Seleccionar" ShowSelectButton="true" ButtonType="Button"/>
+           </Columns>
+           <FooterStyle BackColor="#FFFFCC" ForeColor="#330099" />
+           <HeaderStyle BackColor="#990000" Font-Bold="True" ForeColor="#FFFFCC" />
+           <PagerStyle BackColor="#FFFFCC" ForeColor="#330099" HorizontalAlign="Center" />
+           <RowStyle BackColor="White" ForeColor="#330099" />
+           <SelectedRowStyle BackColor="#FFCC66" Font-Bold="True" ForeColor="#663399" />
+           <SortedAscendingCellStyle BackColor="#FEFCEB" />
+           <SortedAscendingHeaderStyle BackColor="#AF0101" />
+           <SortedDescendingCellStyle BackColor="#F6F0C0" />
+           <SortedDescendingHeaderStyle BackColor="#7E0000" />
+       </asp:GridView>
+   </asp:Panel>
+
+    <asp:Panel ID="gridActionsPanel" runat="server">
+        <asp:LinkButton ID="editarLinkButton" runat="server" OnClick="editarLinkButton_Click">Editar</asp:LinkButton>
+        <asp:LinkButton ID="eliminarLinkButton" runat="server" OnClick="eliminarLinkButton_Click" >Eliminar</asp:LinkButton>
+        <asp:LinkButton ID="nuevoLinkButton" runat="server" OnClick="nuevoLinkButton_Click" >Nuevo</asp:LinkButton>
+    </asp:Panel>
+
+    <asp:Panel ID="formPanel" runat="server">
+        <asp:Label ID="IDPlanLabel" runat="server" Text="ID Plan"></asp:Label>
+        <asp:TextBox ID="IDPlanTextBox" runat="server" ReadOnly="True"></asp:TextBox>
+        <br/>
+
+        <asp:Label ID="DescripcionLabel" runat="server" Text="Descripcion"></asp:Label>
+        <asp:TextBox ID="DescripcionTextBox" runat="server"></asp:TextBox>
+         <asp:RequiredFieldValidator ID="descripReq" runat="server" ErrorMessage="La descripcion del Plan no puede estar vacía"
+         ControlToValidate ="DescripcionTextBox" ForeColor="Red">#</asp:RequiredFieldValidator>
+        <br/>
+
+        <asp:Label ID="IDEspecialidadLabel" runat="server" Text="ID Especialidad"></asp:Label>
+        <asp:TextBox ID="IDEspecialidadTextBox" runat="server"></asp:TextBox>
+        <asp:RequiredFieldValidator ID="IDEspReq" runat="server" ErrorMessage="El ID de la Especialidad del Plan no puede estar vacío"
+         ControlToValidate ="IDEspecialidadTextBox" ForeColor="Red">#</asp:RequiredFieldValidator>
+        <br />
+
+        <asp:ValidationSummary ID="Errores" ForeColor="Red" runat="server"/>
+        <br />
+    </asp:Panel>
+
+    <asp:Panel ID="formActionsPanel" runat="server">
+        <asp:LinkButton ID="aceptarLinkButton" runat="server" OnClick="aceptarLinkButton_Click" >Aceptar</asp:LinkButton>
+        <asp:LinkButton ID="cancelarLinkButton" runat="server" >Cancelar</asp:LinkButton>
+    </asp:Panel>
+
+</asp:Content>
+
---
+++ UI.WebForm/Planes.aspx.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Business.Entities;
+using Business.Logic;
+
+namespace UI.Web
+{
+    public partial class Planes : System.Web.UI.Page
+    {
+        PlanLogic _logic;
+        private PlanLogic Logic
+        {
+            get
+            {
+                if (_logic == null)
+                {
+                    _logic = new PlanLogic();
+                }
+                return _logic;
+            }
+        }
+        public enum FormModes
+        {
+            Alta,
+            Baja,
+            Modificacion
+        }
+        public FormModes FormMode
+        {
+            get { return (FormModes)this.ViewState["FormMode"]; }
+            set { this.ViewState["FormMode"] = value; }
+        }
+        private Plan Entity 
+        {
+            get;
+            set;
+        }
+        private int SelectedID
+        {
+            get
+            {
+                if (this.ViewState["SelectedID"] != null)
+                {
+                    return (int)this.ViewState["SelectedID"];
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            set
+            {
+                this.ViewState["SelectedID"] = value;
+            }
+        }
+        private bool IsEntitySelected
+        {
+            get { return (this.SelectedID != 0); }
+        }
+        private void LoadGrid()
+        {
+            this.gridView.DataSource = this.Logic.GetAll(); // cambiar y traer las especialidades
+            this.gridView.DataBind();
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.Page.IsPostBack)
+            {
+                this.LoadGrid();
+            }
+        }
+
+        protected void gridView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.SelectedID = (int)this.gridView.SelectedValue;
+        }
+
+        protected void LoadForm(int id)
+        {
+            this.Entity = this.Logic.GetOne(id);
+            this.IDPlanTextBox.Text = this.Entity.ID.ToString();
+            this.DescripcionTextBox.Text = this.Entity.Descripcion;
+            this.IDEspecialidadTextBox.Text = this.Entity.IDEspecialidad.ToString();
+        }
+
+        protected void editarLinkButton_Click(object sender, EventArgs e)
+        {
+            if (this.IsEntitySelected)
+            {
+                this.formPanel.Visible = true;
+                this.FormMode = FormModes.Modificacion;
+                this.LoadForm(this.SelectedID);
+                this.EnableForm(true);
+            }
+        }
+
+        protected void LoadEntity(Plan plan)
+        {
+            plan.ID = int.Parse(this.IDPlanTextBox.Text);
+            plan.IDEspecialidad = int.Parse(this.IDEspecialidadTextBox.Text);
+            plan.Descripcion = this.DescripcionTextBox.Text;
+        }
+
+        protected void SaveEntity(Plan plan)
+        {
+            this.Logic.Save(plan);
+        }
+
+        protected void aceptarLinkButton_Click(object sender, EventArgs e)
+        {
+            switch (this.FormMode)
+            {
+                case FormModes.Baja:
+                    this.DeleteEntity(this.SelectedID);
+                    this.LoadGrid();
+                    break;
+                case FormModes.Modificacion:
+                    this.Entity = new Plan();
+                    this.Entity.ID = this.SelectedID;
+                    this.Entity.State = BusinessEntity.States.Modified;
+                    this.LoadEntity(this.Entity);
+                    this.SaveEntity(this.Entity);
+                    this.LoadGrid();
+                    break;
+                case FormModes.Alta:
+                    this.Entity = new Plan();
+                    this.LoadEntity(this.Entity);
+                    this.SaveEntity(this.Entity);
+                    this.LoadGrid();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void EnableForm(bool enable)
+        {
+            this.DescripcionTextBox.Enabled = enable;
+            this.IDEspecialidadTextBox.Enabled = enable;
+        }
+
+        protected void eliminarLinkButton_Click(object sender, EventArgs e)
+        {
+            if (this.IsEntitySelected)
+            {
+                this.formPanel.Visible = true;
+                this.FormMode = FormModes.Baja;
+                this.EnableForm(false);
+                this.LoadForm(this.SelectedID);
+            }
+        }
+
+        private void DeleteEntity(int id)
+        {
+            this.Logic.Delete(id);
+        }
+
+        protected void nuevoLinkButton_Click(object sender, EventArgs e)
+        {
+            this.formPanel.Visible = true;
+            this.FormMode = FormModes.Alta;
+            this.ClearForm();
+            this.EnableForm(true);
+        }
+
+        private void ClearForm()
+        {
+            this.IDPlanTextBox.Text = string.Empty;
+            this.IDEspecialidadTextBox.Text = string.Empty;
+            this.DescripcionTextBox.Text = string.Empty;
+        }
+    }
+}
