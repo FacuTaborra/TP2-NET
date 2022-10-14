709552bc10d1e2d707791dfa45a80eccc58f917a
lucia cappll <luciacappll@gmail.com>
2022-10-14 01:03:42 -0300

    ABMC UI.WebForm Comisiones terminado

---
+++ UI.WebForm/Comisiones.aspx
@@ -0,0 +1,60 @@
+﻿<%@ Page Title="Comisiones" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Comisiones.aspx.cs" Inherits="UI.WebForm.Comisiones" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="bodyContentPlaceHolder" runat="server">
+    
+    <asp:Panel ID="grid" runat="server">
+    
+        <asp:GridView ID="GridView" runat="server" AutoGenerateColumns="False" SelectedRowStyle-BackColor="Black" HorizontalAlign="Center" SelectedRowStyle-ForeColor="White" DataKeyNames="ID" OnSelectedIndexChanged="gridView_SelectedIndexChanged" CellPadding="4" ForeColor="#333333" GridLines="None">
+             <AlternatingRowStyle BackColor="White" ForeColor="#284775" />
+            <Columns>
+                <asp:BoundField DataField="ID" HeaderText="ID" ReadOnly="True" />
+                <asp:BoundField DataField="Descripcion" HeaderText="Descripcion" />
+                <asp:BoundField DataField="AnioEspecialidad" HeaderText="Año Especialidad" />
+                <asp:BoundField DataField="Plan" HeaderText="Plan" />
+                <asp:CommandField SelectText="Seleccioar" ShowSelectButton="true" ButtonType="Button"/>
+            </Columns>
+            <EditRowStyle BackColor="#999999" />
+            <FooterStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+            <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+            <RowStyle BackColor="#F7F6F3" ForeColor="#333333" />
+            <SelectedRowStyle BackColor="#E2DED6" Font-Bold="True" ForeColor="#333333" />
+            <SortedAscendingCellStyle BackColor="#E9E7E2" />
+            <SortedAscendingHeaderStyle BackColor="#506C8C" />
+            <SortedDescendingCellStyle BackColor="#FFFDF8" />
+            <SortedDescendingHeaderStyle BackColor="#6F8DAE" />
+        </asp:GridView>
+    </asp:Panel>
+
+    <asp:Panel ID="gridActionPanel" HorizontalAlign="Center"  runat="server">
+        <asp:LinkButton ID="nuevoLinkButton" runat="server" OnClick="nuevoLinkButton_Click" >Nuevo</asp:LinkButton>
+        <asp:LinkButton ID="editarLinkButton" runat="server" OnClick="editarLinkButton_Click" >Editar</asp:LinkButton>
+        <asp:LinkButton ID="eliminarLinkButton" runat="server" OnClick="eliminarLinkButton_Click" >Eliminar</asp:LinkButton>
+    </asp:Panel>
+
+    <asp:Panel ID="formPanel" runat="server">
+        <asp:Label ID="DescripcionLabel" runat="server" Text="Descripcion"></asp:Label>
+        <asp:TextBox ID="txtDescripcion" runat="server"></asp:TextBox>
+        <%--<asp:RequiredFieldValidator ID="DescReq" runat="server" ErrorMessage="Ingrese una descripción" ControlToValidate="txtDescripcion" ForeColor="Red">#</asp:RequiredFieldValidator>--%>
+        <br />
+    
+        <asp:Label ID="AnioEspecialidadLabel" runat="server" Text="Año Especialidad"></asp:Label>
+        <asp:TextBox ID="txtAnioEspecialidad" runat="server"></asp:TextBox>
+        <%--<asp:RequiredFieldValidator ID="anioEspReq" runat="server" ErrorMessage="Ingrese una Año" ControlToValidate="txtAnioEspecialidad" ForeColor="Red">#</asp:RequiredFieldValidator>
+        <asp:RangeValidator ID="anioInRange" runat="server" ErrorMessage="Año Inválido. El año será válido desde 1990 hasta 2030" MaximumValue="2030" MinimumValue="1990" ControlToValidate="txtAnioEspecialidad" ForeColor="Red">#</asp:RangeValidator>--%>
+        <br />
+
+        <asp:Label ID="PlanLabel" runat="server" Text="Plan"></asp:Label>
+        <asp:DropDownList ID="ddlPlanes" runat="server">
+        </asp:DropDownList>
+        <br />
+        
+        <%--<asp:ValidationSummary ID="errores" ForeColor="Red" runat="server"/>--%>
+
+        <asp:Panel ID="formActionPanel" runat="server">
+            <asp:LinkButton ID="aceptarLinkButton" runat="server" OnClick="aceptarLinkButton_Click" >Aceptar</asp:LinkButton>
+            <asp:LinkButton ID="cancelarLinkButton" runat="server" >Cancelar</asp:LinkButton>
+         </asp:Panel>
+   </asp:Panel>
+
+
+</asp:Content>
---
+++ UI.WebForm/Comisiones.aspx.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Business.Entities;
+using Business.Logic;
+
+namespace UI.WebForm
+{
+    public partial class Comisiones : Default
+    {
+        ComisionLogic _logic;
+
+        private ComisionLogic Logic
+        {
+            get
+            {
+                if (_logic == null)
+                {
+                    _logic = new ComisionLogic();
+                }
+                return _logic;
+            }
+        }
+
+        public FormModes FormMode
+        {
+            get { return (FormModes)this.ViewState["FormMode"]; }
+            set { this.ViewState["FormMode"] = value; }
+        }
+
+        private Comision Entity
+        {
+            get; 
+            set;
+        }
+
+
+        private int SelectedID
+        {
+            get
+            {
+                if (this.ViewState["SelectedID"] != null)
+                {
+                    return (int)this.ViewState["SelectedID"];
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            set
+            {
+                this.ViewState["SelectedID"] = value;
+            }
+        }
+
+        private bool IsEntitySelected
+        {
+            get
+            {
+                return (this.SelectedID != 0);
+            }
+        }
+
+        private void LoadGrid()
+        {
+            this.GridView.DataSource = this.Logic.GetAll();
+            this.GridView.DataBind();
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.Page.IsPostBack)
+            {
+                LoadGrid();
+                LoadDropDown();
+            }
+        }
+        private void LoadDropDown()
+        {
+            PlanLogic pl = new PlanLogic();
+            List<Plan> listaPlanes = new List<Plan>();
+            listaPlanes = pl.GetAll();
+            Plan extraPlan = new Plan();
+            extraPlan.ID = 0;
+            extraPlan.Descripcion = "Elegir Plan";
+            listaPlanes.Add(extraPlan);
+            this.ddlPlanes.DataSource = listaPlanes;
+            this.ddlPlanes.DataValueField = "ID";
+            this.ddlPlanes.DataTextField = "Descripcion";
+            this.ddlPlanes.DataBind();
+            this.ddlPlanes.SelectedValue = 0.ToString();
+        }
+
+        private void LoadForm(int id)
+        {
+            this.Entity = this.Logic.GetOne(id);
+            this.DescripcionLabel.Text = Entity.Descripcion;
+            this.txtDescripcion.Text = Entity.Descripcion;
+            this.txtAnioEspecialidad.Text = Entity.AnioEspecialidad.ToString();
+            this.ddlPlanes.SelectedValue = Entity.Plan.ID.ToString();
+        }
+
+        private void LoadEntity(Comision com)
+        {
+            com.Descripcion = this.txtDescripcion.Text;
+            com.AnioEspecialidad = int.Parse(this.txtAnioEspecialidad.Text);
+            int idPlan = int.Parse(this.ddlPlanes.SelectedValue);
+            PlanLogic pl = new PlanLogic();
+            com.Plan = pl.GetOne(idPlan);
+            this.txtDescripcion.Text = com.Plan.Descripcion;
+            
+        }
+
+        private void SaveEntity(Comision com)
+        {
+            this.Logic.Save(com);
+        }
+
+        private void DeleteEntity(int id)
+        {
+            this.Logic.Delete(id);
+        }
+
+        private void EnableForm(bool enable)
+        {
+            this.txtAnioEspecialidad.Enabled = enable;
+            this.txtDescripcion.Enabled = enable;
+            this.ddlPlanes.Enabled = enable;
+        }
+
+        private void ClearForm()
+        {
+            this.txtAnioEspecialidad.Text = string.Empty;
+            this.txtDescripcion.Text = string.Empty;
+            this.ddlPlanes.SelectedValue = 0.ToString();
+        }
+
+        protected void gridView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.SelectedID = (int)this.GridView.SelectedValue;
+        }
+
+        protected void editarLinkButton_Click(object sender, EventArgs e)
+        {
+            if (IsEntitySelected)
+            {
+                this.formPanel.Visible = true;
+                this.FormMode = FormModes.Modificacion;
+                this.LoadForm(this.SelectedID);
+            }
+        }
+
+        protected void eliminarLinkButton_Click(object sender, EventArgs e)
+        {
+            if (this.IsEntitySelected)
+            {
+                this.formPanel.Visible = true;
+                this.FormMode = FormModes.Baja;
+                this.EnableForm(false);
+                this.LoadForm(this.SelectedID);
+            }
+        }
+        protected void nuevoLinkButton_Click(object sender, EventArgs e)
+        {
+            this.formPanel.Visible = true;
+            this.FormMode = FormModes.Alta;
+            this.ClearForm();
+            this.EnableForm(true);
+            
+        }
+        protected void aceptarLinkButton_Click(object sender, EventArgs e)
+        {
+            switch (this.FormMode)
+            {
+                case FormModes.Baja:
+                    this.DeleteEntity(this.SelectedID);
+                    this.LoadGrid();
+                    break;
+                case FormModes.Modificacion:
+                    this.Entity = new Comision();
+                    this.Entity.ID = this.SelectedID;
+                    this.Entity.State = BusinessEntity.States.Modified;
+                    this.LoadEntity(this.Entity);
+                    this.SaveEntity(this.Entity);
+                    this.LoadGrid();
+                    break;
+                case FormModes.Alta:
+                    this.Entity = new Comision();
+                    this.Entity.State = BusinessEntity.States.New;
+                    this.LoadEntity(this.Entity);
+                    this.SaveEntity(this.Entity);
+                    this.LoadGrid();
+                    break;
+                default:
+                    break;
+            }
+            this.formPanel.Visible = false;
+        }
+    }
+}
