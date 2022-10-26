d4d8ed509cc718baff176c7d631b365ddc694a76
FacuTaborra <taborrafacundo@gmail.com>
2022-10-26 18:52:02 -0300

    guardo

--- UI.Desktop/PlanEstudio.Designer.cs
+++ UI.Desktop/PlanEstudio.Designer.cs
@@ -30,13 +30,13 @@ namespace UI.Desktop
         private void InitializeComponent()
         {
             this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.label1 = new System.Windows.Forms.Label();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.dgvMateriasPlan = new System.Windows.Forms.DataGridView();
-            this.btnActualizar = new System.Windows.Forms.Button();
-            this.btnSalir = new System.Windows.Forms.Button();
             this.AñoCalendario = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Materia = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.label1 = new System.Windows.Forms.Label();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
             this.toolStripContainer1.ContentPanel.SuspendLayout();
             this.toolStripContainer1.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
@@ -51,13 +51,23 @@ namespace UI.Desktop
             this.toolStripContainer1.ContentPanel.BackColor = System.Drawing.Color.CadetBlue;
             this.toolStripContainer1.ContentPanel.Controls.Add(this.label1);
             this.toolStripContainer1.ContentPanel.Controls.Add(this.tableLayoutPanel1);
-            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(494, 349);
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(488, 356);
             this.toolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.toolStripContainer1.Location = new System.Drawing.Point(0, 25);
             this.toolStripContainer1.Name = "toolStripContainer1";
-            this.toolStripContainer1.Size = new System.Drawing.Size(494, 349);
+            this.toolStripContainer1.Size = new System.Drawing.Size(488, 381);
             this.toolStripContainer1.TabIndex = 1;
             this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(153, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Plan de Estudio";
             // 
             // tableLayoutPanel1
             // 
@@ -68,12 +78,12 @@ namespace UI.Desktop
             this.tableLayoutPanel1.Controls.Add(this.btnActualizar, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.btnSalir, 1, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 38);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 45);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(494, 311);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(488, 311);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // dgvMateriasPlan
@@ -86,27 +96,8 @@ namespace UI.Desktop
             this.dgvMateriasPlan.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dgvMateriasPlan.Location = new System.Drawing.Point(3, 3);
             this.dgvMateriasPlan.Name = "dgvMateriasPlan";
-            this.dgvMateriasPlan.Size = new System.Drawing.Size(488, 273);
+            this.dgvMateriasPlan.Size = new System.Drawing.Size(482, 273);
             this.dgvMateriasPlan.TabIndex = 0;
-            // 
-            // btnActualizar
-            // 
-            this.btnActualizar.Anchor = System.Windows.Forms.AnchorStyles.Right;
-            this.btnActualizar.Location = new System.Drawing.Point(297, 282);
-            this.btnActualizar.Name = "btnActualizar";
-            this.btnActualizar.Size = new System.Drawing.Size(94, 26);
-            this.btnActualizar.TabIndex = 1;
-            this.btnActualizar.Text = "Actualizar";
-            this.btnActualizar.UseVisualStyleBackColor = true;
-            // 
-            // btnSalir
-            // 
-            this.btnSalir.Location = new System.Drawing.Point(397, 282);
-            this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(94, 26);
-            this.btnSalir.TabIndex = 2;
-            this.btnSalir.Text = "Salir";
-            this.btnSalir.UseVisualStyleBackColor = true;
             // 
             // AñoCalendario
             // 
@@ -120,20 +111,29 @@ namespace UI.Desktop
             this.Materia.Name = "Materia";
             this.Materia.ReadOnly = true;
             // 
-            // label1
+            // btnActualizar
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(153, 7);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(187, 31);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Plan de Estudio";
+            this.btnActualizar.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnActualizar.Location = new System.Drawing.Point(291, 282);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(94, 26);
+            this.btnActualizar.TabIndex = 1;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(391, 282);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(94, 26);
+            this.btnSalir.TabIndex = 2;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
             // 
             // PlanEstudio
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.ClientSize = new System.Drawing.Size(494, 374);
+            this.ClientSize = new System.Drawing.Size(488, 406);
             this.Controls.Add(this.toolStripContainer1);
             this.Name = "PlanEstudio";
             this.Controls.SetChildIndex(this.toolStripContainer1, 0);
